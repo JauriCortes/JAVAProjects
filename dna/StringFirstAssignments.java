861734a9b77ebde0c8888d8661f817002326e86c
jauri <jauricardenas18@gmail.com>
2025-04-29 22:25:25 -0500

    ocurrences implemented

--- dna/StringFirstAssignments.java
+++ dna/StringFirstAssignments.java
@@ -1,5 +1,7 @@
 package dna;
 
+import java.util.concurrent.BlockingQueue;
+
 public class StringFirstAssignments {
 
     public class part1 {
@@ -52,6 +54,9 @@ public class StringFirstAssignments {
 
         StringFirstAssignments.part2 pr2 = new StringFirstAssignments().new part2();
         pr2.testSimpleGene();
+
+        StringFirstAssignments.part3 pr3 = new StringFirstAssignments().new part3();
+        pr3.testing();
     }
 
     public class  part2 {
@@ -107,4 +112,45 @@ public class StringFirstAssignments {
         }
         
     }
+
+    public class  part3 {
+    
+        public Boolean twoOcurrences(String stringa, String stringb) {
+
+            int ocurrences = 0;
+            int index = 0;
+
+            while ((index = stringa.indexOf(stringb, index)) != -1) {     
+                ocurrences++;
+                index++;
+            }
+            
+            if (2 > ocurrences) {
+                return false;
+            }
+            return true;
+        }
+
+        public void testing () {
+            String[][] testCases = {
+                {"by", "A story by Abby Long"},
+                {"a", "banana"},
+                {"atg", "ctgtatgta"}
+            };
+    
+            for (String[] testCase  : testCases) {
+    
+                String stringa = testCase[1];
+                String stringb = testCase[0];
+                System.out.println("String a: " + stringa);
+                System.out.println("String b: " + stringb);
+    
+                boolean areTheretwoOcurrences = twoOcurrences(stringa, stringb);
+                
+                System.out.println("Are there 2 ocurrences? : " + areTheretwoOcurrences);
+    
+            }
+        }
+    }
+
 }
