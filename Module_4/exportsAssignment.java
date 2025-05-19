24d39ae9cec7122548cd76cbeeb3209b16f2de67
JauriCortes <80365016+JauriCortes@users.noreply.github.com>
2025-05-19 22:30:40 +0000

    apache import

---
+++ Module_4/exportsAssignment.java
@@ -0,0 +1,16 @@
+import edu.duke.*;
+import apache.*;
+
+class exportsAssignment {
+
+    public static void main(String[] args) {
+        
+        System.out.println("hola");
+    }
+
+    public void tester() {
+
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+    }
+}
