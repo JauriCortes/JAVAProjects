eefbb205acf49625339c35a40606582a57acc9dd
jauri <jauricardenas18@gmail.com>
2025-05-24 00:09:14 -0500

    countryInfo

--- Module_4/exportsAssignment.java
+++ Module_4/exportsAssignment.java
@@ -1,16 +1,39 @@
 import edu.duke.*;
-import apache.*;
+import org.apache.commons.csv.*;
 
 class exportsAssignment {
 
     public static void main(String[] args) {
         
-        System.out.println("hola");
+        exportsAssignment ea = new exportsAssignment();
+        ea.tester();
     }
 
     public void tester() {
 
-        FileResource fr = new FileResource();
+        FileResource fr = new FileResource("exports/exports_small.csv");
         CSVParser parser = fr.getCSVParser();
+
+        String info = countryInfo(parser, "Namibia");
+        System.out.println(info);
+    }
+
+    public String countryInfo(CSVParser parser, String country) {
+
+        for(CSVRecord record : parser) {
+            String country_col = record.get("Country");
+
+            if (country_col.contains(country)) {
+                
+                String exports = record.get("Exports");
+                String value = record.get("Value (dollars)");
+
+                String result = country_col + ": " +  exports + ": " +  value + ": ";
+                return result;
+            }
+        }
+
+        return("NOT FOUND");
+        
     }
 }
