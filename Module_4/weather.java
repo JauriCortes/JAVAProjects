6c21e94ffcc8d0ca9a1a3c0dcf7f69e45f4234f2
JauriCortes <80365016+JauriCortes@users.noreply.github.com>
2025-05-30 03:30:00 +0000

    weather

---
+++ Module_4/weather.java
@@ -0,0 +1,19 @@
+public class weather {
+
+    public static void main(String[] args){
+
+    }
+
+    public void testColdestHourInFile() {
+
+        FileResource fr = new FileResource("nc_weather/2014/weather-2014-04-04.csv");
+    
+    }
+
+    public CSVRecord coldestHourInFile(CSVParser parser){
+
+        for (CSVRecord record: parser) {
+            
+        }
+    }
+}
